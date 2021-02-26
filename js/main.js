882d77e87554e27870b385ceda669aea692b97aa
DjekeyeVICHE <djekeyeviche@gmail.com>
2021-02-26 14:33:40 +0100

    Notification banner removed

--- js/main.js
+++ js/main.js
@@ -204,23 +204,5 @@ function CopyToClipboard(text) {
   }
 } 
 
-// Get the modal
-var modal = document.getElementById("banner");
-
-// Get the <span> element that closes the modal
-var span = document.getElementById("close-banner");
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
 window.onload = resize;
 window.onresize = resize;
